c6155d424ffaff411f6777f9e54a00925dfe943e
NimeshNayaju <nimeshnayaju@gmail.com>
2019-02-28 10:11:04 -0400

    Added google oauth Strategy

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const passport = require('passport');
+
+// Load passport config
+require('./config/passport')(passport);
+
+// Load routes
+const auth = require('./routes/auth'); 
 
 const app = express();
 
 app.get('/', (req, res) => {
   res.send('Welcome to the home page!');
 });
+
+// Use routes
+app.use('/auth', auth);
 
 const port = process.env.PORT || 5000;
 
---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI: 'mongodb://nimeshnayaju:n1DtAPWj95Sd0rdJ@cluster0-jfwmp.mongodb.net/test?retryWrites=true',
+  googleClientID: '229361474041-65ipiahdr91ml8d9fas8f8oveaqsqgf1.apps.googleusercontent.com',
+  googleClientSecret: 'JSJuUBNUmEkUWTAREajRgjmn'
+}
